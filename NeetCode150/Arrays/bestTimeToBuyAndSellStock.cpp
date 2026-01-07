134d2b3774c08374cb3681169437db26e43a5527
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-01-07 16:55:52 +0530

    solved maximum Subarray

--- NeetCode150/Arrays/bestTimeToBuyAndSellStock.cpp
+++ NeetCode150/Arrays/bestTimeToBuyAndSellStock.cpp
@@ -23,4 +23,28 @@ public:
         return max_Profit;
         
     }
+};class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>set;
+
+        for(int i = 0; i<nums.size(); i++) {
+            set.insert(nums[i]);
+        }
+
+        if(nums.size() == set.size()) return false;
+        else return true;
+    }
+};class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>set;
+
+        for(int i = 0; i<nums.size(); i++) {
+            set.insert(nums[i]);
+        }
+
+        if(nums.size() == set.size()) return false;
+        else return true;
+    }
 };
